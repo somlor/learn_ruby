da5caa9e805b44d56cc2c04ce27924192edd2afc
sean <somlor@gmail.com>
2013-01-10 03:53:48 -0600

    refactor pig latin code. i *think* this is better. (ಠ_ಠ)

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,22 +1,46 @@
-def translate(str)
-	vowels = %w(a e i o u)
-	consonants_one = %w(b c d f g h j k l m n p q r s t v w x y z)
-	consonants_two = %w(bl br ch cl cr dr fl fr gl gr pl pr qu sc sh sk sl sm sn st sw th tr tw wh wr)
-	consonants_three = %w(sch scr shr sph spl spr squ str thr)
+VOWELS = %w(a e i o u)
+CONSONANTS = %w(b bl br c ch cl cr d dr f fl fr g gl gr h j k l m n p pl pr q qu r s sc sch scr sh shr sk sl sm sn sph spl spr squ st str sw t th thr tr tw v w wh wr x y z)
 
+def vowel_prefix?(word)
+	prefix_match(word, VOWELS)
+end
+
+def prefix_match(needle, haystack)
+	# loop thru distinct prefix sizes within haystack in descending order
+	# if needle is in haystack, return current prefix size
+	# this lets us know how many chars to cut for consonant translation
+	max_len = haystack.max_by(&:length).length
+	max_len.downto(1) do |len|
+		prefix = needle[0..(len - 1)]
+		return len if haystack.include?(prefix)
+	end
+	nil
+end
+
+def translate_vowel(word)
+	word + "ay"
+end
+
+def translate_consonant(word, prefix_size)
+	word[prefix_size..word.length] + word[0..(prefix_size - 1)] + "ay"
+end
+
+def translate(str)
 	piglatin = []
-	words = str.split
-	words.each do |word|
-		case
-		when consonants_three.include?(word[0..2])
-			piglatin << word[3..word.length] + word[0..2] + "ay"
-		when consonants_two.include?(word[0..1])
-			piglatin << word[2..word.length] + word[0..1] + "ay"
-		when consonants_one.include?(word[0])
-			piglatin << word[1..word.length] + word[0] + "ay"
-		when vowels.include?(word[0])
-			piglatin << word + "ay"			
+	str.split.each do |word|
+		if vowel_prefix?(word)
+			piglatin << translate_vowel(word)
+		else
+			prefix_size = prefix_match(word, CONSONANTS)
+			piglatin << translate_consonant(word, prefix_size)
 		end
 	end
 	piglatin.join(" ")
+end
+
+if __FILE__ == $0
+	puts prefix_match("school", CONSONANTS)
+	puts prefix_match("apple", CONSONANTS)
+	puts prefix_match("apple", VOWELS)
+	puts vowel_prefix?("apple")
 end
