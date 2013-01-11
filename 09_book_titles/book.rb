6759a2a923cce211ba19a1abc77f0a59c124a18d
sean <somlor@gmail.com>
2013-01-10 19:58:16 -0600

    completed lab 09 book titles

---
+++ 09_book_titles/book.rb
@@ -0,0 +1,16 @@
+class Book
+	SMALL_WORDS = %w(a an and as at but by for in it nor of on or over the to up)
+
+	attr_reader :title
+
+	def title=(t)
+		@title = titleize(t)
+	end
+
+	def titleize(s)
+		words = s.split
+		words.each { |word| word.capitalize! unless SMALL_WORDS.include?(word) }
+		words.first.capitalize!
+		words.join(" ")
+	end
+end
